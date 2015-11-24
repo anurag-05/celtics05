d1d131fc981faa965fcdec62ea13a1569cf906de
anurag-05 <anujnagarkoti@gmail.com>
2015-11-24 19:58:24 +0530

    part 1

--- Mini Project/src/Frame1.java
+++ Mini Project/src/Frame1.java
@@ -17,19 +17,21 @@ import javax.swing.JScrollBar;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.JSlider;
 
 
-public class Frame1 {
+public class Frame5 {
 
-	private JFrame frame;
-	private JTextField textField1;
-	private JTextField textField2;
+	JFrame frame;
+	JTextField textField2;
 	JTextArea textArea;
 	JScrollPane scroll;
 	JPanel panel_1,panel_2;
-	private JButton btnBack;
-	private JTextArea Header;
+	JButton btnBack;
+	JTextArea Header;
 	JComboBox comboBox;
+	JCheckBox chckbxMale,chckbxFemale;
 
 	/**
 	 * Launch the application.
@@ -38,7 +40,7 @@ public class Frame1 {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Frame1 window = new Frame1();
+					Frame5 window = new Frame5();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,7 +52,7 @@ public class Frame1 {
 	/**
 	 * Create the application.
 	 */
-	public Frame1() {
+	public Frame5() {
 		initialize();
 	}
 
@@ -69,16 +71,6 @@ public class Frame1 {
 		frame.getContentPane().add(panel_1, "name_12195122635849");
 		panel_1.setLayout(null);
 		
-		JLabel lblEnterGen = new JLabel("Enter Male / Female :");
-		lblEnterGen.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblEnterGen.setBounds(112, 120, 170, 16);
-		panel_1.add(lblEnterGen);
-		
-		textField1 = new JTextField();
-		textField1.setBounds(294, 118, 116, 22);
-		panel_1.add(textField1);
-		textField1.setColumns(10);
-		
 	
 		
 		JLabel lblEnterYear = new JLabel("Enter Year :");
@@ -87,7 +79,7 @@ public class Frame1 {
 		panel_1.add(lblEnterYear);
 		
 		textField2 = new JTextField();
-		textField2.setBounds(294, 184, 116, 22);
+		textField2.setBounds(294, 184, 98, 22);
 		panel_1.add(textField2);
 		textField2.setColumns(10);
 		
@@ -95,10 +87,10 @@ public class Frame1 {
 		btnFind.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				String gender,year,f_name,row,name,birth;
+				String gender="",year="",f_name,row,name,birth;
 				String []array;
 				String disp;
-				int flag=0,max,i=0;
+				int flag_y=0,max=0,i=0,flag;
 				
 				panel_1.setVisible(false);
 				panel_2.setVisible(true);
@@ -106,7 +98,6 @@ public class Frame1 {
 				textArea.setText("");
 				Header.setText("");
 				
-				gender = textField1.getText();
 				year = textField2.getText();
 				disp=comboBox.getSelectedItem().toString();
 				System.out.println(disp);
@@ -121,25 +112,33 @@ public class Frame1 {
 					max=50;
 				else
 					max=99999;
-					
 				
-				if(gender.toLowerCase().equals("male") || gender.toLowerCase().equals("female"))
+				if(chckbxMale.isSelected() && chckbxFemale.isSelected())
 				{
-					flag=0;
+					String f1,f2;
+					f1="male_cy"+ year +"_top.csv";
+					f2="female_cy"+ year +"_top.csv";
+					System.out.println(f1);
+					System.out.println(f2);
+					both(f1,f2,year,max);
+					flag_y=1;
+					
+				}else if(chckbxMale.isSelected())
+				{
+					gender="male";
 				}
-				else
+				else if(chckbxFemale.isSelected())
 				{
-					Header.append("Invalid Input");
-					flag=1;
+					gender="female";
 				}
 				
 				if(Integer.parseInt(year)>2013 || Integer.parseInt(year)<1944)
 				{
 					Header.append("Invalid Input");
-					flag=1;
+					flag_y=1;
 				}
 				
-				if(flag==0)
+				if(flag_y==0)
 				{
 					f_name = gender.toLowerCase()+"_cy"+year+"_top.csv";
 					//textArea.append(f_name);
@@ -164,8 +163,8 @@ public class Frame1 {
 							birth=birth.replaceAll("\"","");
 					
 							textArea.append(name);
-							textArea.append("  No of Births ->" );
-							textArea.append(birth);
+							textArea.append("\n  No of Births ->" );
+							textArea.append(birth + "\t" +"(" + gender + ")");
 							textArea.append("\n");
 							i++;
 							
@@ -187,15 +186,30 @@ public class Frame1 {
 		
 		JLabel lblDisplay = new JLabel("Display :");
 		lblDisplay.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblDisplay.setBounds(198, 243, 56, 16);
+		lblDisplay.setBounds(198, 258, 56, 16);
 		panel_1.add(lblDisplay);
 		
 		String[] Display = {"Top 5 Names","Top 10 Names","Top 20 Names","Top 50 Names","All Names"};
 		
 		
 		comboBox = new JComboBox(Display);
-		comboBox.setBounds(294, 241, 116, 22);
+		comboBox.setBounds(294, 256, 116, 22);
 		panel_1.add(comboBox);
+		
+		chckbxMale = new JCheckBox("Male");
+		chckbxMale.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		chckbxMale.setBounds(294, 111, 78, 50);
+		panel_1.add(chckbxMale);
+		
+		chckbxFemale = new JCheckBox("Female");
+		chckbxFemale.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		chckbxFemale.setBounds(388, 111, 98, 50);
+		panel_1.add(chckbxFemale);
+		
+		JLabel lblSelect = new JLabel("Select :\r\n");
+		lblSelect.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSelect.setBounds(198, 111, 56, 50);
+		panel_1.add(lblSelect);
 		
 		panel_2 = new JPanel();
 		frame.getContentPane().add(panel_2, "name_12248119307610");
@@ -225,5 +239,105 @@ public class Frame1 {
 		Header.setBounds(50, 44, 205, 22);
 		panel_2.add(Header);
 		
+	}
+	public void both (String f_name1 , String f_name2 , String year , int display )
+	{
+		int male_births=0 ,female_births=0,i=1,flag=0 ;
+		String []male_data={""},female_data={""};
+		String male_row,female_row;
+		
+		File file1 = new File (f_name1);
+		
+		File file2 = new File (f_name2);
+		
+		try
+		{
+			Scanner read1 = new Scanner (file1);
+			Scanner read2 = new Scanner (file2);
+			
+			for(int z=0;z<2;z++)
+			read1.next(); 
+			
+			
+			for(int z=0;z<2;z++)
+			read2.next();
+			
+			Header.append("YEAR    " + year);
+			
+			do
+			{
+				if(flag!=2)
+				{
+					male_row = read1.next();
+					male_data = male_row.split(",");
+					male_births = convert (male_data[1]);
+				}
+				if(flag!=1)
+				{
+					female_row = read2.next();
+					female_data = female_row.split(",");
+					female_births = convert (female_data[1]);
+				}
+			
+			
+				if(male_births >= female_births)
+				{
+					textArea.append(male_data[0]);
+					textArea.append("\n  No of Births -> " );
+					textArea.append(male_births + "\t" + "(male)\n" );
+					flag=1;
+				}
+				else
+				{
+					textArea.append(female_data[0]);
+					textArea.append("\n  No. of Births -> " );
+					textArea.append(female_births + "\t" + "(female)\n");
+					flag=2;
+				}
+			
+			i++;
+			
+			}while(i<=display && read1.hasNext() && read2.hasNext());
+			
+			if(i<=display)
+			{
+				textArea.append(female_data[0]);
+				textArea.append("\n  No. of Births -> " );
+				textArea.append(female_births + "\t" + "(female)\n");
+				i++;
+			}
+			
+			if(i<=display)
+			{
+				do
+				{
+					female_row = read2.next();
+					female_data = female_row.split(",");
+					female_births = convert (female_data[1]);
+				
+					textArea.append(female_data[0]);
+					textArea.append("\n  No. of Births -> " );
+					textArea.append(female_births + "\t" + "(female)\n");
+				
+					i++;
+				
+				}while(read2.hasNext() && i<=display);
+			}
+			
+			
+			read1.close();
+			read2.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+	public static int convert (String births)
+	{
+		births=births.replaceAll("[^0-9]", "");
+		int new_v = Integer.parseInt(births);
+		return (new_v);	
 	}
 }
